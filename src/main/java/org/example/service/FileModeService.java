1fcc004964be775bbb230591ef516de512810391
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-03-06 15:46:27 +0900

    Refactor to divide services

---
+++ src/main/java/org/example/service/FileModeService.java
@@ -0,0 +1,95 @@
+package org.example.service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.example.repository.StudentFeeRepository;
+
+public class FileModeService {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        System.out.println("\n파일 모드로 전환합니다.");
+
+        try {
+            var fileName = getFileName();
+            var workbook = getWorkbook(fileName);
+            checkFeeByWorkbook(workbook);
+        } catch (FileNotFoundException e) {
+            System.out.println("입력한 파일명의 파일을 찾을 수 없습니다. 확장자까지 입력해주세요.");
+        } catch (IOException e) {
+            System.out.println("파일을 여는 도중 문제가 생겼습니다.");
+        }
+
+        scanner.close();
+    }
+
+    private String getFileName() {
+        System.out.println("납부 확인을 진행할 파일명을 입력해주세요. 확장자까지 입력해주세요. ex) test.xls");
+        return scanner.nextLine();
+    }
+
+    private Workbook getWorkbook(String fileName) throws IOException {
+        var fileInputStream = new FileInputStream(fileName);
+        return fileName.endsWith("xlsx")
+                ? new XSSFWorkbook(fileInputStream)
+                : new HSSFWorkbook(fileInputStream);
+    }
+
+    private void checkFeeByWorkbook(Workbook workbook) {
+        var sheet = workbook.getSheetAt(0);
+        printHeader(sheet);
+
+        var studentIdHeaderIndex = getStudentIdHeaderIndex();
+        var isPayedHeaderIndex = getIsPayedHeaderIndex();
+        var isPayedCharacter = getIsPayedString();
+
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            var row = sheet.getRow(i);
+
+            var isPayed = row.getCell(isPayedHeaderIndex).getStringCellValue();
+            if (!isPayed.equals(isPayedCharacter)) {
+                continue;
+            }
+
+            var studentIdCell = row.getCell(studentIdHeaderIndex);
+            studentIdCell.setCellType(CellType.STRING);
+            var studentId = row.getCell(studentIdHeaderIndex).getStringCellValue();
+            if (StudentFeeRepository.getInstance().getStudentInfo(studentId) == null) {
+                System.out.println(studentId + " - 학생회비 납부 X");
+            }
+        }
+    }
+
+    private void printHeader(Sheet sheet) {
+        var headerRow = sheet.getRow(0);
+
+        for (int i = 0; i < headerRow.getLastCellNum(); i++) {
+            var index = i + 1;
+            System.out.println(index + ". " + headerRow.getCell(i));
+        }
+    }
+
+    private int getStudentIdHeaderIndex() {
+        System.out.println("학번으로 사용할 컬럼의 숫자를 입력해주세요.");
+        return Integer.parseInt(scanner.nextLine()) - 1;
+    }
+
+    private int getIsPayedHeaderIndex() {
+        System.out.println("힉생회비 납부여부로 사용할 컬럼의 숫자를 입력해주세요.");
+        return Integer.parseInt(scanner.nextLine()) - 1;
+    }
+
+    private String getIsPayedString() {
+        System.out.println("힉생회비 납부여부로 판단할 문자열을 넣어주세요. ex) O, 납부함 등등");
+        return scanner.nextLine().trim();
+    }
+
+}
---
+++ src/main/java/org/example/service/ScannerModeService.java
@@ -0,0 +1,41 @@
+package org.example.service;
+
+import java.util.Scanner;
+import org.example.entity.StudentInfo;
+import org.example.repository.StudentFeeRepository;
+
+public class ScannerModeService {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        System.out.println("\n스캐너 모드로 전환합니다.");
+        System.out.println("메뉴로 돌아가기 -> q");
+
+        while (true) {
+            System.out.print("바코드를 스캔하세요: ");
+            String id = scanner.nextLine().trim();
+
+            if (id.equals("q")) {
+                System.out.println("메뉴로 돌아갑니다.");
+                break;
+            }
+
+            StudentInfo student = StudentFeeRepository.getInstance().getStudentInfo(id);
+
+            if (student == null) {
+                System.out.println("해당 학생의 정보가 없습니다.");
+                continue;
+            }
+
+            if (student.isPayedStudentFee) {
+                System.out.println(student.name + "님은 학생회비 납부자입니다.");
+            } else {
+                System.out.println(student.name + "님은 학생회비 미납부자입니다.");
+            }
+        }
+
+        scanner.close();
+    }
+
+}
